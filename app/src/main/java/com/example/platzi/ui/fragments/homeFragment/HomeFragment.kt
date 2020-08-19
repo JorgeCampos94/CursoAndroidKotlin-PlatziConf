e13d43188d4eb54fcebc2badb4b10c92d6ce238d
JorgeCampos94 <12398705Jacko>
2020-08-19 17:23:31 -0500

    build(home fragment) "create home fragment - cards details platzi conf"

--- app/src/main/java/com/example/platzi/ui/fragments/homeFragment/HomeFragment.kt
+++ app/src/main/java/com/example/platzi/ui/fragments/homeFragment/HomeFragment.kt
@@ -1,60 +1,75 @@
 package com.example.platzi.ui.fragments.homeFragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.platzi.R
+import com.example.platzi.adapters.CardsHomeAdapter
+import com.example.platzi.models.CardDescriptionHome
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var recyclerViewCardHome : RecyclerView? = null
+    private var adapterCardHome : CardsHomeAdapter? = null
+    private var listCardHome : MutableList<CardDescriptionHome>? = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val viewCardHome = inflater.inflate(R.layout.fragment_home, container, false)
+        context?.let {
+            initAll(viewCardHome)
+        }
+        return viewCardHome
+    }
+
+    private fun initAll(viewCardHome: View?) {
+        context?.let {
+            getDataCardList()
+            recyclerViewCardHome = viewCardHome?.recyclerView_cards_description_event_home
+            adapterCardHome = CardsHomeAdapter(it)
+            Log.w("GGGG","ENTRO")
+            listCardHome?.let{listCardHome->
+                adapterCardHome?.setBrandsList(listCardHome)
+            }
+            recyclerViewCardHome?.layoutManager = LinearLayoutManager(it, LinearLayoutManager.VERTICAL , false)
+            recyclerViewCardHome?.adapter = adapterCardHome
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+    private fun getDataCardList() {
+        listCardHome?.clear()
+        listCardHome?.add(
+            CardDescriptionHome(
+                R.drawable.banner_platzi_conf_1,
+                getString(R.string.label_question_whats_is_it_platziConf),
+                getString(R.string.label_response_whats_is_it_platziConf)
+            )
+        )
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        listCardHome?.add(
+            CardDescriptionHome(
+                R.drawable.banner_platzi_conf_2,
+                getString(R.string.label_question_why_attend),
+                getString(R.string.label_response_why_attend)
+            )
+        )
+
+        listCardHome?.add(
+            CardDescriptionHome(
+                R.drawable.banner_platzi_conf_3,
+                getString(R.string.label_title_information_platziConf),
+                getString(R.string.label_body_information_platziConf)
+            )
+        )
     }
 }
---
+++ app/src/main/java/com/example/platzi/utility/ExtensionsFunction.kt
@@ -0,0 +1,82 @@
+package com.example.platzi.utility
+
+import android.app.Activity
+import android.content.Context
+import android.os.Build
+import android.text.Selection
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearSmoothScroller
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+fun View.setVisibleOrGone(value:Boolean){
+    this.visibility = if(value) View.VISIBLE else View.GONE
+}
+
+fun ImageView.setGlide(context: Context, value: String, placeHolder: Int){
+    Glide.with(context)
+        .load(value)
+        .placeholder(placeHolder)
+        .error(placeHolder)
+        .into(this)
+}
+
+fun ImageView.setGlide(context: Context, value: Int, placeHolder: Int){
+    Glide.with(context)
+        .load(value)
+        .placeholder(placeHolder)
+        .centerCrop()
+        .error(placeHolder)
+        .into(this)
+}
+
+fun RecyclerView.smoothSnapToPosition(position: Int, snapMode: Int = LinearSmoothScroller.SNAP_TO_END) {
+    val smoothScroller = object : LinearSmoothScroller(this.context) {
+        override fun getVerticalSnapPreference(): Int = snapMode
+        override fun getHorizontalSnapPreference(): Int = snapMode
+    }
+    smoothScroller.targetPosition = position
+    layoutManager?.startSmoothScroll(smoothScroller)
+}
+
+fun Activity.changeColorStatusBar(color:Int){
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        this.window?.let {window->
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = color
+        }
+    }
+}
+
+fun EditText.showKeyBoard(activity: Activity?){
+    val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(this,InputMethodManager.SHOW_IMPLICIT)
+}
+
+fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>) {
+        val spannableString = SpannableString(this.text)
+        for (link in links) {
+            val clickableSpan = object : ClickableSpan() {
+                override fun onClick(view: View) {
+                    Selection.setSelection((view as TextView).text as Spannable, 0)
+                    view.invalidate()
+                    link.second.onClick(view)
+                }
+            }
+            val startIndexOfLink = this.text.toString().indexOf(link.first)
+            spannableString.setSpan(clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length,
+                Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
+        }
+        this.movementMethod = LinkMovementMethod.getInstance() // without LinkMovementMethod, link can not click
+        this.setText(spannableString, TextView.BufferType.SPANNABLE)
+}
